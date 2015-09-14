9f71d521818d94853c0728a92a5be28125827c6a
yongkun.chen <yongkun.chen@juniorchina.com>
2015-09-14 17:27:47 +0800

    Initial Commit

---
+++ MultiIconConfig/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiIconConfig
+//
+//  Created by chenyk on 15/9/14.
+//  Copyright (c) 2015年 chenyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiIconConfig/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultiIconConfig
+//
+//  Created by chenyk on 15/9/14.
+//  Copyright (c) 2015年 chenyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
